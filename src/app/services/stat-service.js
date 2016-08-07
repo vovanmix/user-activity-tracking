78579919650d89c6a66920d52654b6b47a2095ba
vladimir <singingcode@gmail.com>
2016-08-06 18:56:15 -0700

    initial commit

--- src/app/services/stat-service.js
+++ src/app/services/stat-service.js
@@ -1,6 +1,13 @@
 var config = require('../config');
 
-var collectStats = function() {
+
+/**
+ * Collects stats
+ * @param {Date} date_from
+ * @param {Date} date_to
+ * @param {string|number} user_id
+ */
+var collectStats = function(date_from, date_to, user_id) {
     // config.storage;
     //todo
 };
